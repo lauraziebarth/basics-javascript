34392a17473b144850571651b906aa614caf8db5
lauraziebarth <lauragzie@gmail.com>
2021-01-06 15:19:47 -0300

    Valida o status code do  HTTP response

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -7,12 +7,21 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+        var erroAjax = document.querySelector("#erro-ajax");
+        
+        if (xhr.stauts == 200){
+            erroAjax.classList.add("invisivel")
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel")
+        }
     });
 
     xhr.send();
